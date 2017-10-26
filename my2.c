03a97020b55b20cd2caf358cfb2bc418115b1fb4
wangch64 <33135943+wangch64@users.noreply.github.com>
2017-10-27 05:34:24 +0800

    Create my2.c

---
+++ my2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool IsPasswordValid(void);
+int main(void) {
+    bool PWverify;
+
+	puts("Enter your password:");
+	PWverify = IsPasswordValid();
+	if (!PWverify) {
+		puts("Password Error!! Please try again.");
+		return -1;
+	}
+	else puts("Welcome. Your password is correct.");
+	return 0;
+}
+
+bool IsPasswordValid(void) {
+	char Password[12];
+
+	gets(Password);
+	if (!strcmp(Password, "secure pro"))
+		return(true);
+	else return(false);
+}
